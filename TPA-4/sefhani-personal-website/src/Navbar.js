4e885562f0b5ad3700bf36598d73c131b704b421
Stefhani Alba <stefhani@mhs.unsyiah.ac.id>
2022-11-28 00:41:47 +0700

    commit tpa 4

---
+++ TPA-4/sefhani-personal-website/src/Navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1>Stefhani.</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/about">About</a>
+                <a href="/portfolio">Portfolio</a>
+                <a href="/blog">Blog</a>
+            </div>
+        </nav>
+     );
+}
+
+export default Navbar;
